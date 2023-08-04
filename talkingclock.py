37c33aa46903ea0f5587056e2cf3fa6fcf839184
Sanjay Vijay <104655727+SanjayVijay27@users.noreply.github.com>
2023-08-04 09:17:22 -0400

    Add files via upload

--- talkingclock.py
+++ talkingclock.py
@@ -55,7 +55,10 @@ class Solution:
             else:
                 minute_word = ten_to_word[minute//10] + " " + num_to_word[minute%10]
             
-            return "It's " + num_to_word[hour % 12] + " " + minute_word + a
+            if minute_word == "":
+                return "It's " + num_to_word[hour % 12] + a
+            else:
+                return "It's " + num_to_word[hour % 12] + " " + minute_word + a
             
 
 def main():
